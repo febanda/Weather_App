2153b8932899ae1637c5a9516d6735b431e898ee
febanda01 <febanda91@gmail.com>
2019-05-02 18:10:06 -0500

    api call done

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree')
+    let locationTimezone = document.querySelector('.location-timezone')
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude
             lat = position.coords.latitude
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/'
+
+            const api = `${proxy}https://api.darksky.net/forecast/7378f77f32f092b949c593dc54c9d995/${lat},${long}`
+
+            fetch(api)
+            .then(data => {
+                return data.json()
+            })
+            .then(data => {
+                const {temperature, summary} = data.currently
+                //Set DOM Elements from the API
+                temperatureDegree.textContent = temperature
+                temperatureDescription.textContent = summary
+                locationTimezone.textContent = data.timezone
+            })
         })
     }else{
         h1.textContent = "hey you need to enable this!"
