4eebae3b2e9136af3bbd4c9f202970bcfb4fbb80
febanda01 <febanda91@gmail.com>
2019-05-08 17:55:07 -0500

    done

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree')
     let locationTimezone = document.querySelector('.location-timezone')
+    let temperatureSection = document.querySelector('.temperature')
+    const temperatureSpan = document.querySelector('.temperature span')
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -19,14 +21,39 @@ window.addEventListener('load', () => {
                 return data.json()
             })
             .then(data => {
-                const {temperature, summary} = data.currently
+                const {temperature, summary, icon} = data.currently
                 //Set DOM Elements from the API
                 temperatureDegree.textContent = temperature
                 temperatureDescription.textContent = summary
                 locationTimezone.textContent = data.timezone
+
+                //Formula for celsius
+                let celsius = (temperature - 32) * (5/9)
+
+                //Set Icon
+                setIcons(icon, document.querySelector('.icon'))
+
+                //Turn temp to Celsius/Farenheit
+                temperatureSection.addEventListener('click', () =>{
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C"
+                        temperatureDegree.textContent = Math.floor(celsius)
+                    }else{
+                        temperatureSpan.textContent = "F"
+                        temperatureDegree.textContent= temperature
+                    }
+                })
+            
             })
         })
     }else{
         h1.textContent = "hey you need to enable this!"
+    }
+
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color: 'white'})
+        const currentIcon = icon.replace(/-/g, '_').toUpperCase()
+        skycons.play()
+        return skycons.set(iconID, Skycons[currentIcon])
     }
 })
